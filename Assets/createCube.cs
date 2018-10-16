367a2eac77b4f2eb65795592a8d34983fc3a9439
Ju-hsin Chen <Ju-Hsin.Chen@stonybrook.edu>
2018-10-16 16:52:49 -0400

    initial commit

---
+++ Assets/createCube.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class createCube : MonoBehaviour {
+
+    public GameObject cube;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("c"))
+        {
+            var tracker = GameObject.Find("Model");
+            Vector3 trackerPos = tracker.transform.position;
+            Instantiate(cube, trackerPos, Quaternion.identity);
+        }
+	}
+}
